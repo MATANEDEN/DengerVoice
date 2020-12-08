f2173cb49d85439a6e1531b8ffc2c333ba60120c
matan eden <matan_eden@hotmail.co.il>
2020-12-08 20:30:18 +0200

    convert audio format

--- convert_audioformat.py
+++ convert_audioformat.py
@@ -1,13 +1,14 @@
 from pydub import AudioSegment
 
 
-ogg_file = r'C:\Users\dev2\Desktop\db\temp.ogg'
-wav_file = r'C:\Users\dev2\Desktop\db\temp1.wav'
+ogg_file = r'C:\Users\Eden\Desktop\my_new_proj\20_words.ogg'
+wav_file = r'C:\Users\Eden\Desktop\my_new_proj\200_words.wav'
 
 # wave and raw don’t use ffmpeg
-wav_audio = AudioSegment.from_file(r"C:\Users\dev2\Desktop\db\temp.ogg", format="ogg")
-raw_audio = AudioSegment.from_file(r"C:\Users\dev2\Desktop\db\temp.ogg", format="ogg",
+wav_audio = AudioSegment.from_file(r"C:\Users\Eden\Desktop\my_new_proj\20_words.ogg", format="ogg")
+raw_audio = AudioSegment.from_file(r"C:\Users\Eden\Desktop\my_new_proj\20_words.ogg", format="ogg",
                                    frame_rate=44100, channels=2, sample_width=2)
 
-wav_audio.export(r'C:\Users\dev2\Desktop\db\temp1.wav', format="wav")
-raw_audio.export(r'C:\Users\dev2\Desktop\db\temp1.wav', format="wav")
+wav_audio.export(r'C:\Users\Eden\Desktop\my_new_proj\200_words.wav', format="wav")
+raw_audio.export(r'C:\Users\Eden\Desktop\my_new_proj\200_words.wav', format="wav")
+#
