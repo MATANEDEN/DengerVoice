7aa31124c34ec1082e7811b1c90d2a6988d8c349
matan eden <matan_eden@hotmail.co.il>
2021-01-03 23:39:42 +0200

    order_files

---
+++ order_files.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+def order_files_by(main_dir):
+    list_words = []
+    dir_path = os.path.dirname(main_dir)
+    all_files = os.listdir(os.path.dirname(main_dir))
+
+    for i in range(len(all_files)):
+        if all_files[i].startswith('word'):
+            list_words.append(os.path.join(dir_path,all_files[i]))
+
+
+
+    return list_words
+
