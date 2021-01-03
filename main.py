b1e8a8b79bc53988680636382d65a56dff74a84b
matan eden <matan_eden@hotmail.co.il>
2021-01-03 23:41:33 +0200

    some updates

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import os
-# from convert_audioformat import convert_audio_format
-
+from order_files import order_files_by
+from convert_audioformat import convert_audio_format
 from RMS_algo import Preprocess_Signal
-
-
+from keyword_spotting_service import Keyword_Spotting_Service
 
 
 
 if __name__ == '__main__':
 
-    # ogg_path = r'C:\Users\Eden\Downloads\a\part1.ogg'
-    # convert_audio_format(ogg_path)
+    list_text = []
+    ogg_path = r'C:\Users\dev2\Desktop\matan_temp\part1.ogg'
+    convert_audio_format(ogg_path)
 
     main_dir =  ogg_path.replace('ogg','wav')
     Preprocess_Signal(main_dir)
 
+    files  = order_files_by(main_dir)
+    kss = Keyword_Spotting_Service()
+    for i in range(len(files)):
+        list_text.append(kss.predict(files[i]))
 
+    print(list_text)
