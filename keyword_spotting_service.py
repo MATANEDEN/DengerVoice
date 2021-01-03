ab9011e822c6b77a40df441ca171b19a1db1bf34
matan eden <matan_eden@hotmail.co.il>
2021-01-03 23:40:48 +0200

    some updates

--- keyword_spotting_service.py
+++ keyword_spotting_service.py
@@ -7,33 +7,44 @@ SAMPLES_TO_CONSIDER = 22050
 
 class _Keyword_Spotting_Service:
     """Singleton class for keyword spotting inference with trained models.
+
     :param model: Trained model
     """
 
     model = None
     _mapping = [
+        "bed",
         "cat",
-        "dog",
         "down",
+        "eight",
+        "five",
+        "four",
         "go",
+        "happy",
+        "house",
         "left",
-        "marvin",
+        "nine",
         "no",
         "off",
         "on",
-        "right",
+        "one",
+        "seven",
         "sheila",
         "six",
         "stop",
+        "three",
+        "two",
         "up",
+        "wow",
         "yes",
-
+        "zero"
     ]
     _instance = None
 
 
     def predict(self, file_path):
         """
+
         :param file_path (str): Path to audio file to predict
         :return predicted_keyword (str): Keyword predicted by the model
         """
@@ -53,10 +64,12 @@ class _Keyword_Spotting_Service:
 
     def preprocess(self, file_path, num_mfcc=13, n_fft=2048, hop_length=512):
         """Extract MFCCs from audio file.
+
         :param file_path (str): Path of audio file
         :param num_mfcc (int): # of coefficients to extract
         :param n_fft (int): Interval we consider to apply STFT. Measured in # of samples
         :param hop_length (int): Sliding window for STFT. Measured in # of samples
+
         :return MFCCs (ndarray): 2-dim array with MFCC data of shape (# time steps, # coefficients)
         """
 
@@ -75,6 +88,7 @@ class _Keyword_Spotting_Service:
 
 def Keyword_Spotting_Service():
     """Factory function for Keyword_Spotting_Service class.
+
     :return _Keyword_Spotting_Service._instance (_Keyword_Spotting_Service):
     """
 
@@ -83,29 +97,3 @@ def Keyword_Spotting_Service():
         _Keyword_Spotting_Service._instance = _Keyword_Spotting_Service()
         _Keyword_Spotting_Service.model = tf.keras.models.load_model(SAVED_MODEL_PATH)
     return _Keyword_Spotting_Service._instance
-
-
-
-
-if __name__ == "__main__":
-
-    # create 2 instances of the keyword spotting service
-    kss = Keyword_Spotting_Service()
-    # kss1 = Keyword_Spotting_Service()
-
-    # check that different instances of the keyword spotting service point back to the same object (singleton)
-    # assert kss is kss1
-
-    # make a prediction
-    print(kss.predict("new_test/word0.wav"))
-    print(kss.predict("new_test/word1.wav"))
-    print(kss.predict("new_test/word2.wav"))
-    print(kss.predict("new_test/word3.wav"))
-    print(kss.predict("new_test/word4.wav"))
-    print(kss.predict("new_test/word5.wav"))
-    print(kss.predict("new_test/word6.wav"))
-    print(kss.predict("new_test/word7.wav"))
-    print(kss.predict("new_test/word8.wav"))
-    print(kss.predict("new_test/word9.wav"))
-
-
