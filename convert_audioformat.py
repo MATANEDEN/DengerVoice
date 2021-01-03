c5d87e3fa0efbffdb06d01bd051721c7ee7a2ae3
matan eden <matan_eden@hotmail.co.il>
2021-01-03 23:43:08 +0200

    convert_ogg_to_wav

--- convert_audioformat.py
+++ convert_audioformat.py
@@ -7,7 +7,3 @@ def convert_audio_format(ogg_path):
     wav_audio = AudioSegment.from_file(ogg_path, format="ogg")
 
     wav_audio.export(ogg_path.replace('ogg','wav'), format="wav")
-
-if __name__ == '__main__':
-    ogg_path = r'C:\Users\dev2\Downloads\a\part1.ogg'
-    convert_audio_format(ogg_path)
