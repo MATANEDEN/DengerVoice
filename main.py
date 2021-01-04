57fa57ca35f8c8906cd17d00932180ee9235bc71
matan eden <matan_eden@hotmail.co.il>
2021-01-04 23:31:49 +0200

    add alert box

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from order_files import order_files_by
 from convert_audioformat import convert_audio_format
 from RMS_algo import Preprocess_Signal
 from keyword_spotting_service import Keyword_Spotting_Service
-
+from Alert import alert_massage
 
 
 if __name__ == '__main__':
 
     list_text = []
-    ogg_path = r'C:\Users\dev2\Desktop\matan_temp\part1.ogg'
+    dangerous_word = 'left'
+    ogg_path = r'C:\Users\dev2\Downloads\a\wav2.ogg'
     convert_audio_format(ogg_path)
 
     main_dir =  ogg_path.replace('ogg','wav')
@@ -19,4 +20,9 @@ if __name__ == '__main__':
     for i in range(len(files)):
         list_text.append(kss.predict(files[i]))
 
-    print(list_text)
+    # print(list_text)
+
+    for  j in range(len(list_text)):
+        if list_text[j]==dangerous_word:
+            alert_massage()
+
